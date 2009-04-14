7f86498b65924ac8e6835737a16fc1db09f4d4f8
paulkohler_cp <SND\paulkohler_cp>
2009-04-14 16:09:00 -0700

    ...and more project refactoring...

--- MiniSqlQuery.ConnectionStringsManager.PlugIn/Properties/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("Mini SQL Query - Connection Strings Manager Plugin")]
-[assembly: AssemblyDescription("A Mini SQL Query Plugin for managing the list of connection strings.")]
-[assembly: AssemblyProduct("MiniSqlQuery.ConnectionStringsManager.PlugIn")]
-[assembly: AssemblyVersion("0.9.9.1")]
-[assembly: AssemblyFileVersion("0.9.9.1")]
